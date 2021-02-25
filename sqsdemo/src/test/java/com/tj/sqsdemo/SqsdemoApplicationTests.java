3597f5672cdaaa00fbb879c58ba5e2eb617b1106
TJ <tanujain1@gmail.com>
2021-02-24 21:53:50 -0800

    initial commit

---
+++ sqsdemo/src/test/java/com/tj/sqsdemo/SqsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj.sqsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
